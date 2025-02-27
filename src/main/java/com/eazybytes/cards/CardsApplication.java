43925cceed36c8a00f5e42f7d9b621df74a3c48d
jghan <jghan@mobigen.com>
2025-02-27 20:37:06 +0900

    change

--- src/main/java/com/eazybytes/cards/CardsApplication.java
+++ src/main/java/com/eazybytes/cards/CardsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+// import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.eazybytes.cards.dto.CardsContactInfoDto;
@@ -16,7 +16,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
-@EnableFeignClients
+// @EnableFeignClients
 /*@ComponentScans({ @ComponentScan("com.eazybytes.cards.controller") })
 @EnableJpaRepositories("com.eazybytes.cards.repository")
 @EntityScan("com.eazybytes.cards.model")*/
